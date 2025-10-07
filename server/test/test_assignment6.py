8ea2cfd811c1a0babf3b3ce1be7f01d778848063
darlynvp <vdarlyn21@gmail.com>
2025-10-07 11:28:07 -0700

    Update test_assignment6.py
    added darlyn test case

--- server/test/test_assignment6.py
+++ server/test/test_assignment6.py
@@ -1,4 +1,8 @@
 from src.assignment6 import add
+from src.assignment import squared
 
 def test_add():
     assert add(15, 25) == 40
+
+def test_squared_numbers(self):
+        self.assertEqual(squared(2,3),8)

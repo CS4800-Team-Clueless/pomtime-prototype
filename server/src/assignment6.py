3c285ec9581f862ba28bb2daf92f31eaef80dbaa
Alicia <aliciahuang@cpp.edu>
2025-10-07 17:50:18 -0700

    Added test for getting events on certain date

--- server/src/assignment6.py
+++ server/src/assignment6.py
@@ -1,6 +1,49 @@
+# temporary test db
+calendar = [
+                {
+                    'date': '9/5/2025',
+                    'event': 'Assignment Due'
+                },
+                {
+                    'date': '8/21/2025',
+                    'event': 'First Day of School'
+                },
+                {
+                    'date': '9/1/2025',
+                    'event': 'Labor Day'
+                },
+                {
+                    'date': '10/7/2025',
+                    'event': 'CS4800 Assignment Due'
+                },
+                {
+                    'date': '10/10/2025',
+                    'event': 'Assignment Due'
+                },
+                {
+                    'date': '9/30/2025',
+                    'event': 'Assignment Due'
+                },
+                {
+                    'date': '12/31/2025',
+                    'event': 'Last Day of Year 2025'
+                },
+                {
+                    'date': '12/31/2025',
+                    'event': 'New Year\'s Eve'
+                }
+            ]
+
 def add(a, b):
     sum = a + b
     return sum
 
 def squared(c,d):
     return pow(c,d)
+
+def get_events(date):
+    events = []
+    for event in calendar:
+        if (date == event.get('date')):
+            events.append(event.get('event'))
+    return events
--- server/test/test_assignment6.py
+++ server/test/test_assignment6.py
@@ -1,8 +1,14 @@
 from src.assignment6 import add
 from src.assignment6 import squared
+from src.assignment6 import get_events
 
 def test_add():
     assert add(15, 25) == 40
 
 def test_squared_numbers(self):
-        self.assertEqual(squared(2,3),8)
+    self.assertEqual(squared(2,3),8)
+
+def test_get_events_on_date():
+    assert get_events('9/1/2025') == ['Labor Day']
+    assert get_events('10/7/2025') == ['CS4800 Assignment Due']
+    assert get_events('12/31/2025') == ['Last Day of Year 2025', 'New Year\'s Eve']

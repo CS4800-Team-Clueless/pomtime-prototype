135f3681557c0b57fd2099dfd9439ef4ab12eb51
Kristie Lee <131427987+kristielee04@users.noreply.github.com>
2025-11-27 00:07:05 -0800

    setup for mongodb

--- server/src/app.py
+++ server/src/app.py
@@ -2,9 +2,18 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
+from pymongo import MongoClient
 
 # Load variables from .env file
 load_dotenv()
+
+# Initialize MongoDB client
+MONGODB_URI = os.getenv("MONGODB_URI")
+DB_NAME = os.getenv("MONGODB_DB_NAME", "PomTimeDB")
+
+# Connect to MongoDB
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 CORS(app)

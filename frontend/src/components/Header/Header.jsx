448b37cc043a1e86565c2773cf7b5c3e7d84db42
Kristie Lee <131427987+kristielee04@users.noreply.github.com>
2025-11-30 11:38:29 -0800

    added icon and updated header style

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import { NavLink } from "react-router-dom";
 import ProfileMenu from "../ProfileMenu/ProfileMenu";
 import "./Header.css";
+import pomLogo from "../../assets/icons/PomIcon.png";
 
 export default function Header() {
   return (
     <header className="pt-header">
       <div className="pt-header-inner">
         <NavLink to="/" className="pt-brand">
-           Pom🐕Time!
+          <img src={pomLogo} alt="PomTime Logo" className="pt-logo" />
+          <span className="pt-logo-text">PomTime!</span>
         </NavLink>
         <nav className="pt-nav">
           <NavLink to="/calendar" className="pt-link">

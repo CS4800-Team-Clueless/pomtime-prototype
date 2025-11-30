f3caaf9f7a2cf2961915506bc31b49c67cca522d
Cay Weaks <csweaks@cpp.edu>
2025-11-29 16:38:53 -0800

    add style changes

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -5,8 +5,8 @@ export default function LoginPage() {
   return (
     <div className="login-page">
       <div className="login-container">
-        <h1>Welcome to PomTime</h1>
-        <p>Please sign in with your Google account to continue</p>
+        <h1>Login to PomTime</h1>
+        <p>Please sign in and authenticate with your Google account to continue</p>
         <LoginButton />
       </div>
     </div>

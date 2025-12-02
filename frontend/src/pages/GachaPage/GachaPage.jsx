891f1e4986531e420e565664ffdf536f1722ef59
darlynvp <vdarlyn21@gmail.com>
2025-12-01 17:01:32 -0800

    sound effect for wish pull

--- frontend/src/pages/GachaPage/GachaPage.jsx
+++ frontend/src/pages/GachaPage/GachaPage.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import GachaBanner from '../../components/Gacha/GachaBanner/GachaBanner.jsx';
 import GachaOverlay from '../../components/Gacha/GachaOverlay/GachaOverlay.jsx';
 import './GachaPage.css';
+import sparkleSfx from '../../assets/sound_effects/pull_sparkle_effect.wav';
 
 export default function GachaPage() {
   const [results, setResults] = useState([]);
   const [showOverlay, setShowOverlay] = useState(false);
+
+  //audio instance and use sparkle effect
+  const sparkleAudioRef = useRef(null);
+  useEffect(() => {
+    sparkleAudioRef.current = new Audio(sparkleSfx);
+  }, []
+  );
+  useEffect(() => { //reset sound effect for next pull
+    if(showOverlay && results.length > 0 && sparkleAudioRef.current){
+      sparkleAudioRef.current.currentTime =0;
+      sparkleAudioRef.current.play().catch((err) => {
+        console.error('Error playing sfx',err);
+      });
+    }
+  }, [showOverlay,result.length]
+  );
 
   const handleClose = () => {
     setShowOverlay(false);
